ddfa0391b68a51d55fa86ed3a2e63b04fed72f16
Robert Nyström <golgafrincham@gmail.com>
2020-07-07 11:28:14 +0200

    added cli and signal_processing to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 
-setup(name='ps_signal',
-      version='0.1',
-      packages=['ps_signal'],
-      entry_points={
-          'console_scripts': [
-              'ps_signal = ps_signal.__main__:main'
-          ]
-      },
+setup(
+    name='ps_signal',
+    version='0.1b1',
+    packages=['ps_signal', 'cli', 'signal_processing'],
+    entry_points={
+        'console_scripts': ['ps_signal = ps_signal.__main__:main']
+    },
 )
