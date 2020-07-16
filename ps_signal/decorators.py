526e00ac77d9f20286e26f06999009f83919d24b
Robert Nyström <golgafrincham@gmail.com>
2020-07-16 11:31:52 +0200

    added experimental decorator

--- ps_signal/decorators.py
+++ ps_signal/decorators.py
@@ -1,9 +1,13 @@
+from functools import wraps
+
+
 class VerbosePrinter:
     def __init__(self, timer: bool = False, text: str = None) -> None:
         self.timer = timer
         self.text = text
 
     def __call__(self, fn):
+        @wraps
         def inner(*args, **kwargs):
             from time import perf_counter
 
@@ -21,3 +25,37 @@ class VerbosePrinter:
             print("="*50)
             return ret
         return inner
+
+
+# Experimental decorator factory for decorating
+# functions with extra attributes.
+def attributes(**attrs):
+    def decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            return f(*args, **kwargs)
+
+        for attr_name, attr_value in attrs.items():
+            setattr(wrapper, attr_name, attr_value)
+
+        return wrapper
+
+    return decorator
+
+
+"""
+
+class A:
+    def __init__(self):
+        self.size = 0
+
+    @attributes(modifies='size')
+    def change_size(self, new):
+        self.size = new
+
+
+test = A()
+print(A.change_size.modifies)
+
+
+"""
