54e3acce8a3cbabb97f0d8ba2e37a2bf938477cf
Robert Nyström <golgafrincham@gmail.com>
2020-07-27 21:58:03 +0200

    skeleton for signal module added

---
+++ ps_signal/signal/plot.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from . import signal
+
+
+def plot_signal(data: pd.DataFrame):
+    pass
--- ps_signal/signal/signal.py
+++ ps_signal/signal/signal.py
@@ -1,20 +1,74 @@
-__all__ = ['Signal', 'load_data', 'split_signal']
+import pandas as pd
+import sys
+import xlrd
+
+__all__ = ['Signal', 'SubSignal']
 
 
 class Signal:
-    def __init__(self):
-        self.a = 20
+    def __init__(self, filename):
+        self._filename = filename
+        self._data = load_data(filename)
+        self._frequency = calculate_sampling_frequency(self._data)
+        self._period = int(round(1 / self._frequency))
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def frequency(self):
+        return self._frequency
+
+    @property
+    def period(self):
+        return self._period
 
 
-def load_data(filename: str) -> Signal:
+class SubSignal(Signal):
+    def __init__(self, signal, interval):
+        if isinstance(signal, Signal):
+            # Create a subset of the signal using split_signal()
+            pass
+        else:
+            return NotImplemented
+
+
+def load_data(filename: str) -> pd.DataFrame:
+    """Loading the data from a file
+
+    :param filename: [description]
+    :type filename: str
+    :return: [description]
+    :rtype: pd.Dataframe
+    """
+    # Formatting of file is separated by ";" and decimals using ","
+    # First two rows are headers.
+    try:
+        data = pd.read_csv(filename, sep=";", decimal=",",
+                           skiprows=[0, 2])
+
+    except (FileNotFoundError, xlrd.biffh.XLRDError, Exception) as error:
+        sys.exit(error)
+    else:
+        data.columns = ["time", "acc"]
+        return data
+
+
+def calculate_sampling_frequency():
     pass
 
 
-def split_signal():
+def split_signal(signal: Signal, start: int, end: int) -> pd.DataFrame:
     """Funktion för att isolera ett subset ur den grundläggande signalen.
     Input är ett Signal-objekt och ett intervall givet i ms. Output är ett
     nytt signalobjekt men med annorlunda sample i.
     """
+    pass
 
 
 def signal_helper():
